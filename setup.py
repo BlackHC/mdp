4d80d7cacfc5354bb1db60cd160d59f473d74306
Andreas Kirsch <blackhc@gmail.com>
2017-09-18 20:25:59 +0100

    Downgrade IPython constraint.

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['gym>=0.9.2', 'numpy', 'matplotlib', 'networkx', 'ipython>=6.2.0', 'ipywidgets', 'typing'],
+    install_requires=['gym>=0.9.2', 'numpy', 'matplotlib', 'networkx', 'ipython>=6.1.0', 'ipywidgets', 'typing'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
